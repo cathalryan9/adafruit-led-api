c86fd92e4c03bc360b10ff96d08022cbdb2348b5
cathalryan9 <cathalryan92@gmail.com>
2016-10-31 17:38:14 +0000

    Add large display parameter

--- config.py
+++ config.py
@@ -13,5 +13,6 @@ DEMO_FILE_PATH = DIRPATH + '/matrix/examples-api-use/'
 LED_IMAGE_VIEWER = DIRPATH + '/matrix/utils/'
 
 GPIO_MAPPING = '--led-gpio-mapping=adafruit-hat'
+LARGE_DISPLAY_PARAMETER = '-L'
 HOST_IP_ADDRESS = '127.0.0.1'
 HOST_PORT = '5000'
