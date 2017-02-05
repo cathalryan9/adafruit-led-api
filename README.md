efb62db87a58b3a753636b469a3b8727276fff82
cathalryan9 <cathalryan92@gmail.com>
2017-02-05 03:53:55 +0000

    Make setup easier

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # adafruit-led-api
 Web API for LED panels powered by Raspberry Pi and Adafruit hat.
+
+The following commands need to be run after repo is cloned (and submodule pulled)
+
+###Run the setup.py script:
+* python setup.py
+
+###Install dependecies
+* sudo apt-get install python-virtualenv
+* virtualenv flask
+* . flask/bin/activate
+* sudo pip install Flask
+* sudo pip install Pillow
+* sudo npm install -g bower
+
+###In the python dir of rpi-rgb-led-api:
+* sudo apt-get update && sudo apt-get install python2.7-dev python-pillow -y
+* make build-python
+* sudo make install-python
+
+###And in the examples-api-use dir:
+* make
+
+###And in utils:
+* sudo apt-get install libgraphicsmagick++-dev libwebp-dev -y
+* make led-image-viewer
--- setup.py
+++ setup.py
@@ -26,15 +26,13 @@ if not os.path.isfile(config.DATABASE_NAME):
         conn.execute('INSERT INTO PARAMETER (NAME, VALUE) VALUES ("-m", "100");')
         conn.execute('INSERT INTO PARAMETER (NAME, VALUE) VALUES ("-R", "0");')
 
-        #Get list of files in uploads
-        files = []
+        print('Adding files from uploads to db')
+        files_array = []
         for root, subdirs, files in os.walk('uploads'):
             for filename in fnmatch.filter(files, '*'):
-                files.append(os.path.join(root, filename))
+                files_array.append(os.path.join(root, filename))
         for file in files:
             filename, file_extension = os.path.splitext(file)
-            print(filename)
-            print(file_extension)
             conn.execute('INSERT INTO FILE (NAME, TYPE) VALUES ("'+filename+'","'+file_extension+'");')
 
 else:
@@ -46,31 +44,10 @@ else:
 
 # TODO: Make all files. Look in all directories for Makefile. Make if there is one.
 try:
-    print('Making files for rpi-rgb-led-matrix library')
-    os.system('sudo apt-get install python-virtualenv')
-    os.system('virtualenv flask')
-    os.system('. flask/bin/activate')
-    os.system('sudo pip install Flask')
-    os.system('sudo pip install Pillow')
-    os.system('sudo npm install -g bower')
-    os.system('bower init')
-    os.system('npm init')
-    os.system('npm install --save-dev bower')
+
+    os.system('npm install')
     os.system('bower install')
-    os.chdir('rpi-rgb-led-matrix')
-    os.system('make')
-    os.chdir('python')
-    os.system('sudo apt-get update && sudo apt-get install python2.7-dev python-pillow -y')
-    os.system('make build-python')
-    os.system('sudo make install-python')
-    os.chdir('../examples-api-use')
-    os.system('make')
-    os.chdir('../utils')
-    os.system('sudo apt-get install libgraphicsmagick++-dev libwebp-dev -y')
-    os.system('make led-image-viewer')
-
-
 
 except Exception as e:
-    print ('Failed to make files')
+    print ('Failed to install required dependencies')
     print(e)
